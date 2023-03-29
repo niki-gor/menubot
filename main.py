7249599576aba20fd491f57788f2b0a5921742d7
niki-gor <nic.gor@yandex.ru>
2023-03-29 23:17:04 +0300

    Refactor code

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from aiogram import Bot, Dispatcher
 from aiogram.utils import executor
 
 from config_reader import config
-from dispatcher import setup_dispatcher
-from models import Page
+from handler import setup_dispatcher
+from models import HandlerConfig, Page
 
 if __name__ == "__main__":
     with open("pages_config.yml") as f:
         data = yaml.safe_load(f)
 
-    pages = [Page(**page) for page in data]
+    handler_config = HandlerConfig(**data)
 
     bot = Bot(token=config.bot_token.get_secret_value())
     dp = Dispatcher(bot)
-    setup_dispatcher(dp, pages)
+    setup_dispatcher(dp, handler_config)
 
-    executor.start_polling(dp, skip_updates=False)
+    executor.start_polling(dp, skip_updates=True)
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, AnyHttpUrl
+from pydantic import BaseModel, AnyHttpUrl, UUID4
 
 
 class UrlButton(BaseModel):
@@ -9,3 +9,13 @@ class UrlButton(BaseModel):
 class Page(BaseModel):
     name: str
     keyboard: list[UrlButton]
+
+
+# class PageWithId(Page):
+#     page_id: str
+
+
+class HandlerConfig(BaseModel):
+    home_button_text: str
+    main_menu_text: str
+    pages: list[Page]
